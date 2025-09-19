94bec41709b05f39008981b29adac6a788cb2bff
krundhall <kevin.grundhall01@gmail.com>
2025-09-19 21:59:15 +0200

    Winrate and Rank functionality

--- logic.py
+++ logic.py
@@ -1,21 +1,52 @@
 import requests
 import urllib.parse
 
-api_key = "RGAPI-065fb448-0a61-4018-9539-037ecf04ca7c"
+api_key = "RGAPI-6f9070f0-18b2-4559-8b82-b417ec4072ac"
+
 
 def get_puuid(riot_id: str) -> str:
-    ### split into gameName and tagLine
+    ### split into gameName and tagLine, then encode
     game_name, tag_line = riot_id.split("#")
-
-    ### encode, urllib
     game_name = urllib.parse.quote(game_name)
     tag_line = urllib.parse.quote(tag_line)
-
     ### build url
     url = f"https://europe.api.riotgames.com/riot/account/v1/accounts/by-riot-id/{game_name}/{tag_line}"
-    
-    ### get and index for ppuid
+    ### request
     request_puuid = requests.get(url, headers={"X-Riot-Token": api_key})
     data = request_puuid.json()
-    puuid = data["puuid"]
-    return puuid
+    return data["puuid"]
+
+def get_summonerid(puuid: str) -> str:
+    url = f"https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}"
+    request_summonerid = requests.get(url, headers={"X-Riot-Token": api_key})
+    data = request_summonerid.json()
+    return data["id"]
+
+def get_rankedStats(puuid: str) -> dict:
+    url = f"https://euw1.api.riotgames.com/lol/league/v4/entries/by-puuid/{puuid}"
+    request_rankedStats = requests.get(url, headers={"X-Riot-Token": api_key})
+    data = request_rankedStats.json()
+    for i in data:
+        if i["queueType"] == "RANKED_SOLO_5x5":
+            return i
+    return None
+
+def winrate(solo_list: dict) -> float:
+    wi = solo_list["wins"]
+    lo = solo_list["losses"]
+    wr = wi / (wi+lo) * 100 
+    print(f"Winrate: {wr:.2f}%")
+    return wr
+
+def rank_status(puuid: str) -> str:
+    url = f"https://euw1.api.riotgames.com/lol/league/v4/entries/by-puuid/{puuid}"
+    requests_rankedStats = requests.get(url, headers={"X-Riot-Token": api_key})
+    data = requests_rankedStats.json()
+    
+    for i in data:
+        if i["queueType"] == "RANKED_SOLO_5x5":
+            tier = i["tier"]
+            rank = i["rank"]
+            lp = i["leaguePoints"]
+            return f"{tier} {rank} {lp} LP"
+    return "Play some SoloQ shitter"
--- main.py
+++ main.py
@@ -1,6 +1,32 @@
-from logic import get_puuid
+from logic import get_puuid, get_rankedStats, winrate, rank_status
 
+stop = False
+while not stop:
+    print("Hi : ^)\nWelcome to RAKOE\nWhat can I do you for?")
+    print("1) Winrate")
+    print("2) Rank")
+    print("q) Quit")
 
-print("Hi and welcome to Riot ID Fetcher")
-riot_id = input("Enter name:\nex. 'Kevin Egg#EUW'\n>>> ")
-puuid = get_puuid(riot_id)
+    menu_choice = input(">>> ")
+
+    if menu_choice == "q":
+        print("Bye, bye - welcome back!")
+        stop = True
+    
+    elif menu_choice == "1":
+        riot_id = input("Enter name:\nex. 'Kevin Egg#EUW'\n>>> ")
+        puuid = get_puuid(riot_id)
+        solo_stats = get_rankedStats(puuid)
+        if solo_stats:
+            winrate(solo_stats)
+
+    elif menu_choice == "2":
+        riot_id = input("Enter name:\nex. 'Kevin Egg#EUW'\n>>> ")
+        puuid = get_puuid(riot_id)
+        print(rank_status(puuid))
+
+    else:
+        print("Not a valid menu choice\nTry again!")
+    
+    if not stop:
+        input("\nPress Enter to continue...")
